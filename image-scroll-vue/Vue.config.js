326cebc9dba966a6f1bcf58cd30ad70ebeb906ed
林振辉 <315713565@163.com>
2019-03-21 21:49:36 +0800

    image-scroll-vue

---
+++ image-scroll-vue/Vue.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  // 基本路径
+  publicPath: process.env.NODE_ENV === "production" ? '/demo/image-scroll-vue/dist/' : '/',
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+  // 服务器端口号
+  devServer: {
+    port: 8080,
+  },
+}
---
+++ image-scroll-vue/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <ImageScroll :images="images" height="292px" />
+  </div>
+</template>
+
+<script>
+import ImageScroll from '@lin09/image-scroll-vue'
+
+export default {
+  name: 'app',
+  components: {
+    ImageScroll
+  },
+  data() {
+    return {
+      images: [{
+        imgUrl: 'https://gw.alicdn.com/imgextra/i4/1/O1CN01fFFjmR1BsUtzbDibn_!!1-0-lubanu.jpg_790x10000Q75.jpg_.webp',
+        href: ''
+      },{
+        imgUrl: 'https://gw.alicdn.com/imgextra/i1/124/O1CN01QTazoU1CmpcKM3axV_!!124-0-luban.jpg_790x10000Q75.jpg_.webp'
+      },{
+        imgUrl: 'https://gw.alicdn.com/imgextra/i1/50/O1CN01x3WLUL1CEwJJq7Esw_!!50-0-lubanu.jpg_790x10000Q75.jpg_.webp'
+      },{
+        imgUrl: 'https://gw.alicdn.com/imgextra/i3/103/O1CN01IALwmx1CdDImsIMWb_!!103-0-lubanu.jpg_790x10000Q75.jpg_.webp'
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 50px 0 0;
+}
+</style>
+
